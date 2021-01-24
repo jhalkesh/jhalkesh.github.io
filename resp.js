c19e9294d0fcc942af56b85ec91b2e1b2e3457a2
jhalkesh <53436298+jhalkesh@users.noreply.github.com>
2021-01-24 13:53:44 +0530

    my portfolio

---
+++ resp.js
@@ -0,0 +1,7 @@
+burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+navlist = document.querySelector('.nav-list')
+burger.addEventListener('click', ()=>{
+    navbar.classList.toggle('h-nav');
+    navlist.classList.toggle('v-class');
+})
